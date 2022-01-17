568016720b9ad1598a98ae0164c6ecc3e7111900
cedricgeissmann <cedric.geissmann@gmail.com>
2022-01-17 17:39:24 +0000

    Added minimal template

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html", data="data")
 
 if __name__ == "__main__":
     app.run()
